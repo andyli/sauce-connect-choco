5ff65b0f61dbeb34edf130f75fc4db78f0d3e607
Andy Li <andy@onthewings.net>
2018-03-05 14:31:54 +0800

    update to 4.4.11

--- chocolateyInstall.ps1
+++ chocolateyInstall.ps1
@@ -1,11 +1,11 @@
-$version = "sc-4.4.10-win32"
+$version = "sc-4.4.11-win32"
 $packDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 Install-ChocolateyZipPackage `
    -PackageName 'sauce-connect' `
    -UnzipLocation "$packDir" `
    -Url "https://saucelabs.com/downloads/$version.zip" `
-   -Checksum 'c263fb3c16a852f9945c60e4a7ddd80eaa7ed56256df2195e07404ff33edc873' `
+   -Checksum 'e58aef0519c97b83cb8034a1bc07f059770edbee6fb21d16db70b43db6c0ba0f' `
    -ChecksumType 'sha256'
 
 # avoid name conflict with the Windows Service Control (SC)
